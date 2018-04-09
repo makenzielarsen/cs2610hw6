45e76fabec958699bbe60af93558da984f38726b
makenzielarsen <makenzie97larsen@gmail.com>
2018-04-08 22:53:25 -0600

    Adding removal and creating of nodes

--- fib.js
+++ fib.js
@@ -2,44 +2,42 @@
 document.title = "Dynamic Fibonacci Sequence in JavaScript";
 
 // Create a red div in the body
-var div = document.createElement("div");
+let div = document.createElement("div");
 div.setAttribute("class", "red shadowed stuff-box");
 document.querySelector("body").appendChild(div);
 
-var slider = document.createElement("INPUT");
+let slider = document.createElement("INPUT");
 slider.setAttribute("type", "range");
 slider.setAttribute("max", "50");
 slider.setAttribute("value", "25");
 
-var text = document.createElement("p");
+let text = document.createElement("p");
 text.textContent = "Fib(" + slider.value + ")";
+text.id = "text";
 div.appendChild(text);
 
 slider.oninput = function() {
-  var replace = document.createElement("p");
-  text.textContent = "Fib(" + this.value + ")";
-  nums_text.textContent = fib_string(slider.value);
-
-  // var replace = document.createElement("p");
-  // var replace_nums = document.createElement("p");
-  // replace.textContent = "Fib(" + this.value + ")";
-  // replace_nums.textContent = fib_string(slider.value);
-  // div.removeChild(text);
-  // div.removeChild(slider);
-  // div.removeChild(nums_text);
-  // div.appendChild(replace);
-  // div.appendChild(slider);
-  // div.appendChild(replace_nums);
+  let replace = document.createElement("p");
+  replace.id = "text";
+  replace.textContent = "Fib(" + this.value + ")";
+  let replace_nums = document.createElement("p");
+  replace_nums.id = "nums";
+  replace_nums.textContent = fib_string(slider.value);
+  div.removeChild(document.getElementById("text"));
+  div.removeChild(document.getElementById("nums"));
+  div.insertBefore(replace, slider);
+  div.appendChild(replace_nums);
 };
 
 div.appendChild(slider);
 
-var nums_text = document.createElement("p");
+let nums_text = document.createElement("p");
 nums_text.textContent = fib_string(slider.value);
+nums_text.id = "nums";
 div.appendChild(nums_text);
 
 function fibonacci(num) {
-  var a = 1,
+  let a = 1,
     b = 0,
     temp;
 
@@ -54,7 +52,7 @@ function fibonacci(num) {
 }
 
 function fib_string(num) {
-  var string = "1";
+  let string = "1";
   for (i = 1; i <= num; i++) {
     string += ",\xa0\xa0\xa0\xa0  " + fibonacci(i).toString();
   }
