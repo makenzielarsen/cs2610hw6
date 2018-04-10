3b05f98cfcb0088d1b0a64cbfd3f92189749b6e0
makenzielarsen <makenzie97larsen@gmail.com>
2018-04-10 17:08:40 -0600

    Improvements

--- fib.js
+++ fib.js
@@ -11,50 +11,94 @@ slider.setAttribute("type", "range");
 slider.setAttribute("max", "50");
 slider.setAttribute("value", "25");
 
-let text = document.createElement("p");
-text.textContent = "Fib(" + slider.value + ")";
-text.id = "text";
-div.appendChild(text);
+div.appendChild(slider);
 
-slider.oninput = function() {
+var cache = [0, 1];
+
+let updateTopBox = function() {
   let replace = document.createElement("p");
   replace.id = "text";
-  replace.textContent = "Fib(" + this.value + ")";
+  replace.textContent = "Fib(" + slider.value + ")";
   let replace_nums = document.createElement("p");
   replace_nums.id = "nums";
   replace_nums.textContent = fib_string(slider.value);
-  div.removeChild(document.getElementById("text"));
-  div.removeChild(document.getElementById("nums"));
+  if (document.getElementById("text") != undefined) {
+    div.removeChild(document.getElementById("text"));
+  }
+  if (document.getElementById("nums") != undefined) {
+    div.removeChild(document.getElementById("nums"));
+  }
   div.insertBefore(replace, slider);
   div.appendChild(replace_nums);
 };
 
-div.appendChild(slider);
+slider.oninput = updateTopBox;
+updateTopBox();
 
-let nums_text = document.createElement("p");
-nums_text.textContent = fib_string(slider.value);
-nums_text.id = "nums";
-div.appendChild(nums_text);
+let div2 = document.createElement("div");
+div2.setAttribute("class", "red shadowed stuff-box");
+document.querySelector("body").appendChild(div2);
 
-function fibonacci(num) {
-  let a = 1,
-    b = 0,
-    temp;
+let slider2 = document.createElement("INPUT");
+slider2.setAttribute("type", "range");
+slider2.setAttribute("max", "11");
+slider2.setAttribute("value", "4");
 
-  while (num >= 0) {
-    temp = a;
-    a = a + b;
-    b = temp;
-    num--;
+div2.appendChild(slider2);
+
+let updateBottomBox = function() {
+  let replace = document.createElement("p");
+  replace.id = "text2";
+  replace.textContent = "Fib(" + slider2.value + ")";
+  let replace_nums = document.createElement("div");
+  replace_nums.setAttribute("class", "fib");
+  replace_nums.id = "nums2";
+  // let complete_tree = fibonacci2(slider2.value);
+  // console.log(complete_tree);
+  if (document.getElementById("text2") != undefined) {
+    div2.removeChild(document.getElementById("text2"));
+  }
+  if (document.getElementById("nums2")) {
+    div2.removeChild(document.getElementById("nums2"));
   }
 
-  return b;
+  // for (i = 0; i < slider2.value * 2 + 1; i++) {
+  //   if (i === 0) {
+  //     replace_nums.textContent = complete_tree[0];
+  //   }
+  //   j = i;
+  //   while (complete_tree[2 * j] != undefined) {
+  //     let temp = document.createElement("div");
+  //     temp.setAttribute("class", "fib-left");
+  //     temp.textContent = complete_tree[2 * j];
+  //     j *= 2;
+  //   }
+  //   j = i;
+  //   while (complete_tree[2 * i + 1] != undefined) {
+  //     let temp = document.createElement("div");
+  //     temp.setAttribute("class", "fib-right");
+  //     temp.textContent = complete_tree[j * 2 + 1];
+  //     j = j * 2 + 1;
+  //   }
+  // }
+
+  div2.insertBefore(replace, slider2);
+  div2.appendChild(replace_nums);
+};
+
+slider2.oninput = updateBottomBox;
+updateBottomBox();
+
+function fibonacci(num) {
+  if (cache[num]) {
+    return;
+  }
+  for (let i = cache.length; i <= num; i++) {
+    cache[i] = cache[i - 1] + cache[i - 2];
+  }
 }
 
 function fib_string(num) {
-  let string = "1";
-  for (i = 1; i <= num; i++) {
-    string += ",\xa0\xa0\xa0\xa0  " + fibonacci(i).toString();
-  }
-  return string;
+  fibonacci(num);
+  return cache.slice(0, num + 1).join(",\xa0\xa0\xa0\xa0  ");
 }
